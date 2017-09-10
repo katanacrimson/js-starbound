068c8e466cccd901d28d9079cd27857e2da40262
Damian Bushong <katana@odios.us>
2017-09-10 00:33:02 -0500

    Update header comments

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 // ---
 // @copyright (c) 2017 Damian Bushong <katana@odios.us>
 // @license MIT license
-// @url <https://github.com/damianb/>
-// @reddit <https://reddit.com/u/katana__>
+// @url <https://github.com/damianb/js-starbound>
 //
 /*jslint node: true, asi: true */
 'use strict'
