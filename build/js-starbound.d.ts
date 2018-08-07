fa6bedcb942fa758379708bc0c3ee484b5797d6e
Damian Bushong <katana@odios.us>
2018-08-07 11:27:02 -0500

    rebuild, apparently forgot

--- build/js-starbound.d.ts
+++ build/js-starbound.d.ts
@@ -1,3 +1,4 @@
 export { SBON } from 'SBON';
 export { SBVJ01 } from 'SBVJ01';
 export { SBAsset6 } from 'SBAsset6';
+export { ConsumableBuffer, ConsumableFile, ConsumableResource, ExpandingBuffer, ExpandingFile, ExpandingResource, StreamPipeline } from 'ByteAccordion';
